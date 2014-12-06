bcf25a403e8a19f8cfec8d960edacc1902bf075f
Dvergar <laurent.rd@gmail.com>
2014-12-06 16:41:15 +0100

    gitmodules => self-contained

--- sample-openfl/Source/Common.hx
+++ sample-openfl/Source/Common.hx
@@ -10,9 +10,7 @@ class MovementSystem extends System
 
     public override function processEntity(entity:Entity)
     {
-        trace("entity update " + entity.id);
         entity.get(CPosition).x += 0.1;
-        trace("compopos " + entity.get(CPosition).x);
         em.net.markChanged(entity, entity.get(CPosition));
     }
 }
