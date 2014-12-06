7418f53b4da9f59d3b693f01f0a453450652b143
Dvergar <laurent.rd@gmail.com>
2014-12-06 18:34:02 +0100

    package messing up

--- src/osis/yec/sample/Main.hx
+++ src/osis/yec/sample/Main.hx
@@ -1,18 +1,22 @@
-import osis.EntityManager;
+// import osis.EntityManager;
 
 
 class Main
 {
     public function new()
     {
-        var em = new EntityManager();
+        // var em = new EntityManager();
 
-        var player = em.createPlayer();
-        var pos = player.get(CPosition);
-        trace("pos " + pos.x);
+        // var player = em.createPlayer();
+        // var pos = player.get(CPosition);
+        // trace("pos " + pos.x);
 
-        pos = em.addComponent(player, new CPosition());
-        trace("pos " + pos.x);
+        // pos = em.addComponent(player, new CPosition());
+        // trace("pos " + pos.x);
+
+        #if lol
+        trace("lol");
+        #end
     }
 
     public static function main()
