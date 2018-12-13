d8a6f5d220f8817b5d60991c286deb4564ae2477
Dvergar <laurent.rd@gmail.com>
2018-12-13 10:07:55 +0100

    typo

--- build_doc.bat
+++ build_doc.bat
@@ -1,2 +1,1 @@
-haxe doc.hxml && haxelib run dox -i docs -in osis -o D:\Prog\Python\myportfolio\export\osis -D description "You're
-looking at the latest doc, for 0.5.0 doc please add /0.5.0/ to the address"
+haxe doc.hxml && haxelib run dox -i docs -in osis -o D:\Prog\Python\myportfolio\export\osis -D description "You're looking at the latest doc, for 0.5.0 doc please add /0.5.0/ to the address"
